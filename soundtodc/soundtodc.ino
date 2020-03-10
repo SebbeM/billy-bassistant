33885d2a9c55e1254955ce43844a0e666b31da07
Sebastian Magnusson <sebastian.magnusson@tritech.se>
2020-03-10 15:54:53 +0100

    Make body move when receiving digital signal

--- soundtodc/soundtodc.ino
+++ soundtodc/soundtodc.ino
@@ -15,7 +15,7 @@
 
 bool debug = true;
 const int threshold= 5;
-uint8_t timer;
+int timer;
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -25,6 +25,7 @@ Adafruit_DCMotor *mouthMotor = AFMS.getMotor(2); // M2
 
 int SoundPinRight = A0; // Analog in 0
 int SoundPinLeft = A1; // Analog in 1
+int ListeningPin = 7; // Digital in 13
 int Led = LED_BUILTIN;
 
 // The setup routine runs once when you press reset:
@@ -45,25 +46,30 @@ void setup() {
 void loop() {
   // Read the analog input on sound pins:
   int sensorValue = analogRead(SoundPinRight) + analogRead(SoundPinLeft);
+  // Read the digital input on listening pin:
+  int listening = digitalRead(ListeningPin);
   // Map it down to the possible range of  movement:
   sensorValue = map(sensorValue,0,1024,0,255);
   
   analogWrite(Led, sensorValue); // Light up LED according to reading
-
+  
+  if (listening == HIGH) {
+    if (debug) {
+      Serial.println("Listening");
+    }
+    bodyMotor->run(FORWARD);
+    timer = 100;
+  }
+    
   // Move mouth when sound level reaches threshold:
   if (sensorValue > threshold) {
     if (debug) {
       Serial.println(sensorValue);
     }
-    timer = 1000;
-    bodyMotor->run(FORWARD);
     mouthMotor->run(FORWARD);
     delay(30); // Smooth out movement
   } else {
     mouthMotor->run(RELEASE);
-    if (debug) {
-      Serial.println(timer);
-    }
     if (timer == 0) {
       bodyMotor->run(RELEASE);
     } else {
